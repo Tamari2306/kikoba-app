37f46edf16c6c522269a274c457270b6ca818e35
Tamari2306 <tamarsolomon23@gmail.com>
2026-02-16 12:57:07 +0300

    modified site.webmanifest

--- backend/static/service-worker.js
+++ backend/static/service-worker.js
@@ -1,13 +1,20 @@
-const CACHE_NAME = "kikoba-cache-v1";
+const CACHE_NAME = "kikoba-v1";
+const urlsToCache = [
+  "/",
+  "/dashboard",
+  "/offline.html"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.add("/"))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
