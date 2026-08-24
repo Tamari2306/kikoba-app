f7e80a4ae86ed882b25a2668559de955a3367e15
Tamari2306 <tamarsolomon23@gmail.com>
2026-08-24 13:27:56 +0300

    added temporary email address

--- backend/app.py
+++ backend/app.py
@@ -58,7 +58,7 @@ app.config["MAIL_PORT"]           = int(os.environ.get("MAIL_PORT", 587))
 app.config["MAIL_USE_TLS"]        = True
 app.config["MAIL_USERNAME"]       = os.environ.get("MAIL_USERNAME", "")
 app.config["MAIL_PASSWORD"]       = os.environ.get("MAIL_PASSWORD", "")
-app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME", "noreply@kikoba.app")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME", "tamarsolomon23@gmail.com") #change later for transactional email
 mail = Mail(app) if MAIL_AVAILABLE else None
 ts   = URLSafeTimedSerializer(app.config["SECRET_KEY"]) if TOKENS_AVAILABLE else None
 
